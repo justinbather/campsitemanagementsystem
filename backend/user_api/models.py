3c2c4f2a771482ab28e65ff5f39b83b2a71aa463
justinbather <justinbather@hotmail.com>
2023-07-27 13:07:04 -0400

    user model added

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=30, unique=False)
+    last_name = models.CharField(max_length=30, unique=False)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
