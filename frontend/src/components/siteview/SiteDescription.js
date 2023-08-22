e6171879b869ab905a938cb42e2d836124224ec1
Cooper <cooper.macgregor14@gmail.com>
2023-08-21 21:12:44 -0400

    added night calculater on pricing card

--- frontend/src/components/siteview/SiteDescription.js
+++ frontend/src/components/siteview/SiteDescription.js
@@ -3,21 +3,23 @@ import PricingCard from "./PricingCard";
 
 const SiteDescription = (props) => {
   return (
-    <div className="flex flex-row justify-between pt-4 mt-3 w-full h-[20rem] bg-base-100 rounded-xl drop-shadow-lg
-    ">
+    <div className="flex flex-row justify-between pt-4 mt-3 w-full h-[20rem] bg-base-100 rounded-xl drop-shadow-lg">
       <div className="w-1/2 h-full">
-      
-      <div className="flex justify-center gap-3">
-        <div className="w-[6rem] h-6 my-5 px-5 rounded-xl bg-base-100 drop-shadow-lg text-center hover:ring-2 ring-neutral-300">
-              <p className="text-sm text-dark">Sewage</p>
+        <div className="flex justify-center gap-3">
+          <div className="w-[6rem] h-6 my-5 px-5 rounded-xl bg-base-100 drop-shadow-lg text-center hover:ring-2 ring-neutral-300">
+            <p className="text-sm text-dark">Sewage</p>
           </div>
           <div className="w-[6rem] h-6 my-5 px-5 rounded-xl bg-base-100 drop-shadow-lg text-center hover:ring-2 ring-neutral-300">
             <p className="text-sm text-dark">Hydro</p>
           </div>
         </div>
       </div>
-      <div className="flex -1/2 h-full justify-center">
-        <PricingCard site={props.site} initialArrival={props.initialArrival} initialDeparture={props.initialDeparture}/>
+      <div className="flex h-full justify-center">
+        <PricingCard
+          site={props.site}
+          initialArrival={props.initialArrival}
+          initialDeparture={props.initialDeparture}
+        />
       </div>
     </div>
   );
--- frontend/src/components/ui/GradientButton.js
+++ frontend/src/components/ui/GradientButton.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const GradientButton = (props) => {
-
   const navigate = useNavigate();
+  const buttonRef = useRef(null);
+
   const handleClick = () => {
-    navigate('/park')
-  }
+    if (props.onClick) {
+      props.onClick();
+    } else {
+      navigate("/park");
+    }
+  };
+
+  const handleMouseMove = (e) => {
+    const button = buttonRef.current;
+    const rect = button.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    button.style.setProperty("--gradient-x", `${(x / rect.width) * 100}%`);
+    button.style.setProperty("--gradient-y", `${(y / rect.height) * 100}%`);
+  };
+
+  const handleMouseLeave = () => {
+    const button = buttonRef.current;
+    button.style.setProperty("--gradient-x", "50%");
+    button.style.setProperty("--gradient-y", "50%");
+  };
 
   return (
-    <button class="btn btn-wide btn-active bg-gradient-to-r from-[#fec051] to-[#fd4b31] text-base-100 border-0 hover:ring-4 ring-neutral-300" onClick={() => handleClick()}>
-     {props.buttonText}
+    <button
+      ref={buttonRef}
+      className="btn btn-wide btn-active relative overflow-hidden bg-gradient-to-r text-base-100 border-0 hover:ring-4 ring-neutral-300"
+      onClick={handleClick}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      <span
+        className="absolute inset-0 bg-gradient-from-mouse"
+        style={{
+          background: `linear-gradient(to right, ${
+            props.fromColor || "#fec051"
+          }, ${props.toColor || "#fd4b31"})`,
+          backgroundPosition: `var(--gradient-x, 50%) var(--gradient-y, 50%)`,
+          backgroundSize: "200% 200%",
+          transition: "background-position 0.4s ease-out",
+        }}
+      />
+      <span className="relative z-10">{props.buttonText}</span>
     </button>
   );
 };
