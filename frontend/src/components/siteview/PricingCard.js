c860b2a45975bfbc98abf24ef953f10165f7bcec
Cooper <cooper.macgregor14@gmail.com>
2023-08-22 22:31:35 -0400

    chnaged some styling things, added total cost to pricingcard, fixed hover on parkhome, added new if to number of nights to make select dates display if night = 0

--- frontend/src/components/siteview/PricingCard.js
+++ frontend/src/components/siteview/PricingCard.js
@@ -13,12 +13,10 @@ const PricingCard = (props) => {
   const [departureDate, setDepartureDate] = useState(dayjs(initialDeparture));
   const [arrivalDate, setArrivalDate] = useState(dayjs(initialArrival));
   const [numberOfNights, setNumberOfNights] = useState("Select dates");
-  const [netCost, setNetCost] = useState("")
-  const [serviceFee, setServiceFee] = useState("")
-  const [taxes, setTaxes] = useState("")
-  const [total, setTotal] = useState("")
-
-
+  const [netCost, setNetCost] = useState("");
+  const [serviceFee, setServiceFee] = useState("");
+  const [taxes, setTaxes] = useState("");
+  const [total, setTotal] = useState("");
 
   const calcNights = (arrivalDate, departureDate) => {
     const date1String = dayjs(arrivalDate.$d).format("YYYY-MM-DD").toString();
@@ -27,50 +25,51 @@ const PricingCard = (props) => {
     const dayjsDate2 = dayjs(date2String);
     const nights = Math.ceil(dayjsDate2.diff(dayjsDate1, "day"));
     return nights;
-
   };
 
   const calcNetCost = (nights, price) => {
-    const netCost = Math.ceil(nights * price)
-    return netCost
-  }
-  
+    const netCost = Math.ceil(nights * price);
+    return netCost;
+  };
+
   const calcServiceFee = (netCost) => {
-    const serviceRate = .2
-    const serviceFee = Math.ceil(netCost * serviceRate)
-    return serviceFee
-  }
+    const serviceRate = 0.2;
+    const serviceFee = Math.ceil(netCost * serviceRate);
+    return serviceFee;
+  };
 
   const calcTaxes = (netCost, serviceFee) => {
-    const taxes = Math.ceil((netCost + serviceFee) * .13)
-    return taxes
-  }
+    const taxes = Math.ceil((netCost + serviceFee) * 0.13);
+    return taxes;
+  };
 
   const calcTotal = (netCost, taxes) => {
-    const total = Math.ceil(netCost + taxes)
-    return total
-  }
-
-
-  
+    const total = Math.ceil(netCost + taxes);
+    return total;
+  };
 
   useEffect(() => {
-   setNumberOfNights(calcNights(arrivalDate, departureDate))
-   setNetCost(calcNetCost(numberOfNights, props.site.price)) //showing $0 or Nan on load
-   setServiceFee(calcServiceFee(netCost)) //showing $0 or Nan on load
-   setTaxes(calcTaxes(netCost, serviceFee)) //showing $0 or Nan on load
-   setTotal(calcTotal(netCost, taxes)) //showing $0 or Nan on load
-  },[arrivalDate, departureDate, props, netCost, serviceFee, taxes, total]) //adding these dependencies fixed the refresh delay but onload shows 0 or NaN
- 
+    setNumberOfNights(calcNights(arrivalDate, departureDate));
+    setNetCost(calcNetCost(numberOfNights, props.site.price)); //showing $0 or Nan on load
+    setServiceFee(calcServiceFee(netCost)); //showing $0 or Nan on load
+    setTaxes(calcTaxes(netCost, serviceFee)); //showing $0 or Nan on load
+    setTotal(calcTotal(netCost, taxes)); //showing $0 or Nan on load
+  }, [arrivalDate, departureDate, props, netCost, serviceFee, taxes, total]); //adding these dependencies fixed the refresh delay but onload shows 0 or NaN
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <div className="w-3/4 h-full bg-base-100 rounded-xl drop-shadow-lg">
+      <div className="h-full bg-base-100 rounded-xl drop-shadow-lg p-3 border border-neutral-200">
         <div>
           <h1 className="text-xl text-[#fd4b31] pt-3 pl-5">
             ${props.site.price}/night
           </h1>
-          <p className="text-sm pl-5">{numberOfNights > 1 ? numberOfNights + " " + "nights" : numberOfNights + " " + "night"}</p>
+          <p className="text-sm pl-5 pt-2">
+            {numberOfNights === 0
+              ? "Select dates"
+              : numberOfNights > 1
+              ? numberOfNights + " " + "nights"
+              : numberOfNights + " " + "night"}
+          </p>
         </div>
 
         <div className="flex flex-row">
@@ -94,31 +93,38 @@ const PricingCard = (props) => {
             />
           </div>
         </div>
-        <div className="w-full flex flex-row">
-        <div className="w-1/2 flex-col text-left justify-start pl-5">
-            
-                <h3>${props.site.price} x {numberOfNights} nights</h3>
-                <h3>Service fee</h3>
-                <h3>Taxes</h3>
-            
-        </div>
+        <div className="w-full flex flex-row py-4">
+          <div className="w-1/2 flex-col text-left justify-start pl-5">
+            <h3 className="text-neutral-500">
+              ${props.site.price} x {numberOfNights} nights
+            </h3>
+            <h3 className="text-neutral-500">Service fee</h3>
+            <h3 className="text-neutral-500">Taxes</h3>
+          </div>
 
-        <div className="w-1/2 flex-col text-right justify-end pr-5">
-            <h3>${netCost} CAD</h3>
-            <h3>${serviceFee} CAD</h3>
-            <h3>${taxes} CAD</h3>
-
+          <div className="w-1/2 flex-col text-right justify-end pr-5">
+            <h3 className="text-neutral-500">${netCost} CAD</h3>
+            <h3 className="text-neutral-500">${serviceFee} CAD</h3>
+            <h3 className="text-neutral-500">${taxes} CAD</h3>
+          </div>
         </div>
+        <hr className="mx-4 text-neutral-500" />
+        <div className="w-full flex flex-row pb-2">
+          <div className="w-1/2 flex-col text-left justify-start pl-5 pt-2">
+            <h3 className="text-neutral-500">Total</h3>
+          </div>
+          <div className="w-1/2 flex-col text-right justify-end pr-5 pt-2">
+            <h3 className="text-neutral-500">${total} CAD</h3>
+          </div>
         </div>
-        <div className="w-full flex justify-center pt-5">
+        <div className="w-full flex justify-center p-4">
           <CheckoutModal
-            nights = {numberOfNights}
+            nights={numberOfNights}
             site={props.site}
             checkInDate={arrivalDate}
             checkoutDate={departureDate}
           />
         </div>
-        
       </div>
     </LocalizationProvider>
   );
--- frontend/src/components/siteview/SiteDescription.js
+++ frontend/src/components/siteview/SiteDescription.js
@@ -4,18 +4,20 @@ import ListAmenities from "./ListAmenities";
 
 const SiteDescription = (props) => {
   return (
-    <div className="flex flex-row justify-between pt-4 mt-3 w-full h-[20rem] bg-base-100 rounded-xl drop-shadow-lg">
-      <div className="w-1/2 h-full">
+    <div className="flex justify-between w-full mt-10">
+      <div className="flex flex-row justify-between w-8/12 mr-20 h-full border">
         <div className="flex justify-center gap-3">
           <ListAmenities amenities={props.site.amenities} />
         </div>
       </div>
-      <div className="flex h-full justify-center">
-        <PricingCard
-          site={props.site}
-          initialArrival={props.initialArrival}
-          initialDeparture={props.initialDeparture}
-        />
+      <div>
+        <div className="flex h-full justify-center">
+          <PricingCard
+            site={props.site}
+            initialArrival={props.initialArrival}
+            initialDeparture={props.initialDeparture}
+          />
+        </div>
       </div>
     </div>
   );
